92e8f08a11552e4660a41ebb15358dfa68c22969
Navid Mia <navid.mia@outlook.com>
2018-01-24 00:06:13 -0500

    Adds script which detects the color red in a video stream

---
+++ red_color_detection.py
@@ -0,0 +1,51 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2
+import imutils
+import time
+import numpy as np
+
+def main():
+	resolution = (640, 480)
+	fps = 16
+	min_contour_area = 500
+
+	camera = PiCamera()
+	camera.resolution = resolution
+	camera.framerate = fps
+	rawCapture = PiRGBArray(camera, size=resolution)
+	time.sleep(1)
+
+	redLowerBoundary = np.array((0, 0, 100), dtype = "uint8")
+	redUpperBoundary = np.array((80, 80, 250), dtype = "uint8")
+
+	print("Starting video capture, press 'q' to exit")
+	for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	    frame = imutils.resize(f.array, width=500)
+	    mask = cv2.inRange(frame, redLowerBoundary, redUpperBoundary)
+	    output = cv2.bitwise_and(frame, frame, mask = mask)
+
+	    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    
+	    contours = contours[0] if imutils.is_cv2() else contours[1]
+
+	    for contour in contours:
+	        if cv2.contourArea(contour) < min_contour_area:
+	            continue
+
+	        (x, y, w, h) = cv2.boundingRect(contour)
+	        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+
+	    cv2.imshow("Video Feed", frame)   
+	    # cv2.imshow("Color Feed", output)
+	    # cv2.imshow("Mask", mask)
+	    rawCapture.truncate(0)
+
+	    key = cv2.waitKey(1) & 0xFF
+	    if key == ord("q"):
+	        break
+
+	cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+	main()
